e1c3910fc868fcda4afc347bb51b902813e3f001
nghiadanh26 <49084392+nghiadanh26@users.noreply.github.com>
2019-03-30 11:00:04 +0700

    Add files via upload

---
+++ 6sw_v2.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+
+def myNetwork():
+
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='10.0.0.0/8')
+
+    info( '*** Adding controller\n' )
+    c0=net.addController(name='c0',
+                      controller=RemoteController,
+                      ip='127.0.0.1',
+                      protocol='tcp',
+                      port=6633)
+
+    info( '*** Add switches\n')
+    s4 = net.addSwitch('s4', cls=OVSKernelSwitch)
+    s6 = net.addSwitch('s6', cls=OVSKernelSwitch)
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
+    s5 = net.addSwitch('s5', cls=OVSKernelSwitch)
+    s3 = net.addSwitch('s3', cls=OVSKernelSwitch)
+
+    info( '*** Add hosts\n')
+    h4 = net.addHost('h4', cls=Host, ip='10.0.0.4', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
+    h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute=None)
+    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
+
+    info( '*** Add links\n')
+    net.addLink(h1, s1)
+    net.addLink(s1, s4)
+    net.addLink(h2, s2)
+    net.addLink(h3, s3)
+    net.addLink(s2, s4)
+    net.addLink(s3, s5)
+    net.addLink(s4, s6)
+    net.addLink(s5, s6)
+    net.addLink(s6, h4)
+
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+    net.get('s4').start([c0])
+    net.get('s6').start([c0])
+    net.get('s1').start([c0])
+    net.get('s2').start([c0])
+    net.get('s5').start([c0])
+    net.get('s3').start([c0])
+
+    info( '*** Post configure switches and hosts\n')
+
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
+
